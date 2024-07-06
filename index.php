597be465ec49a8adc3e94910463128b36f4dab36
Felipe Maciel <felipemaciel1501@hotmail.com>
2024-07-06 17:41:53 -0300

    atualizado

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
             $real = $_REQUEST["din"] ?? 0;
             $dolar = $real / $cotação;
             $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY); 
-            echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a " . numfmt_format_currency($padrao, $dolar, "USD");
+            echo "<p>Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrao, $dolar, "USD") . "</strong></p>";
         ?>
         <button onclick="javascript:history.go(-1)">Voltar</button>
     </main>
